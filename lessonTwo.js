852b31ec9ac280f89fa764967abbb7ff97150c6f
ViktorKov <kovalskij8@yandex.ru>
2021-11-24 21:58:29 +0300

    Урок 2

--- lessonTwo.js
+++ lessonTwo.js
@@ -26,12 +26,72 @@ alert(b); //3 результат предидущих действий над п
 
 // Чему будут равны переменные x и a в примере ниже? Написать почему так произошло
 // (описать последовательность действий).
-let a = 2;
+/*let a = 2;
 let x = 1 + (a *= 2);
 alert(a);
-alert(x);
+alert(x);*/
 
 // x = 5, a = 4
 /*
 1. в скобках a = 2 * 2
 2. x = 1 + 4*/
+
+// Задание 3
+
+/*
+let a = -4;
+let b = -6;
+
+function math(c,d) {
+    if (c >= 0 && d >= 0){
+        alert(c - d);
+    }
+    else if (c <= 0 && d <= 0){
+        alert(c * d);
+    }else alert(a + b);
+}
+
+math(a,b)*/
+
+//Задание 4
+
+function multi(a, b) {
+    return (a * b);
+}
+
+function div(a, b) {
+    return (a / b);
+}
+
+function addition(a, b) {
+    return (a + b);
+}
+
+function sub(a, b) {
+    return (a - b);
+}
+
+//Задание 5
+
+function mathOperation(arg1, arg2, operation) {
+    switch (operation) {
+        case "+":
+            alert(addition(arg1, arg2))
+            break;
+        case "-":
+            alert(sub(arg1, arg2))
+            break;
+        case "*":
+            alert(multi(arg1, arg2))
+            break;
+        case "/":
+            alert(div(arg1, arg2))
+            break;
+        default: alert("Нет такой математической операции")
+    }
+}
+
+mathOperation(5,3,"+")
+
+
+
