fd3c1ff61a568c7d207c655a7fc89f64f74fe0a3
ViktorKov <kovalskij8@yandex.ru>
2021-12-16 00:11:36 +0300

    Урок 7

--- lessonSeven/3 уровень/1-tabs/app.js
+++ lessonSeven/3 уровень/1-tabs/app.js
@@ -14,31 +14,49 @@ const texts = {
 
 let txt = document.querySelector('.text');
 let navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(value => {
+    value.addEventListener('click', clickHandler);
+})
+
 /**
  * Обработчик клика по .nav-link
- * @param {MouseEvent} event 
+ * @param {MouseEvent} event
  */
 function clickHandler(event) {
     // здесь вызывайте changeText и changeActiveClass, и передавайте
     // им объект события.
-   
+    changeActiveClass(event);
+    changeText(event);
+
+
 }
 
 /**
  * Эта функция должна убирать .active у предыдущего .nav-link и ставить его
  * на тот, по которому кликнули.
- * @param {MouseEvent} event 
+ * @param {MouseEvent} event
  */
 function changeActiveClass(event) {
-    
+    document.querySelector('.active').classList.remove('active');
+    event.target.classList.add('active');
 }
 
 /**
- * Эта фукнция должна читать текст (например через textContent) из 
+ * Эта фукнция должна читать текст (например через textContent) из
  * .nav-link по которому кликнули и в зависимости от этого в .text
  * ставить соответствующий текст из texts.
- * @param {MouseEvent} event 
+ * @param {MouseEvent} event
  */
 function changeText(event) {
-    
+    switch (event.target.textContent) {
+        case 'Link 1':
+            txt.textContent = texts.text1
+            break;
+        case 'Link 2':
+            txt.textContent = texts.text2
+            break;
+        case 'Link 3':
+            txt.textContent = texts.text3
+            break;
+    }
 }
